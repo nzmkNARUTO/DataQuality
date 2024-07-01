319ddb4b0dacbd2bce3bf1333adc476854b0549a
nzmkNARUTO <nzmkNARUTO@outlook.com>
2024-07-01 03:41:58 +0000

    nodes generate dataset themselves

--- node.py
+++ node.py
@@ -1,15 +1,33 @@
+import math
 import torch
 
-from model import trainModel
-from shapley import DShapley, GShapley, looScore
+from copy import deepcopy
+from model import train_model, test_model
+from data_utils import (
+    VolumeDataset,
+    generate_synthetic_data,
+    generate_linear_label,
+    generate_weight_bias,
+)
+from volume import Volume
 
 
 class Node:
     def __init__(
         self,
         baseModel: torch.nn.Module,
+        dimension: int,
+        size: int,
+        weight: torch.Tensor,
+        bias: torch.Tensor,
     ):
         self.baseModel = baseModel
+        self.dimension = dimension
+        self.size = size
+        self.weight = weight
+        self.bias = bias
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.dataset = None
 
     def send(self):
         return self.baseModel.state_dict()
@@ -20,88 +38,49 @@ class Node:
     def train(self):
         raise NotImplementedError
 
+    def generate_dataset(self):
+        x = generate_synthetic_data(d=self.dimension, size=self.size)
+        y = generate_linear_label(
+            x, d=self.dimension, weight=self.weight, bias=self.bias
+        )
+        self.dataset = VolumeDataset(x, y)
+
 
 class Client(Node):
     def __init__(
         self,
         baseModel: torch.nn.Module,
-        x_train: torch.Tensor,
-        y_train: torch.Tensor,
-        x_test: torch.Tensor,
-        y_test: torch.Tensor,
-        lossFunction: torch.nn.Module,
+        loss_func: torch.nn.Module,
         metric: torch.nn.Module,
-        errorThreshold: float,
-        truncatedRounds: int,
-        seed: int,
-        truncatedNumber: int,
+        weight: torch.Tensor,
+        bias: torch.Tensor,
+        dimension: int,
+        size: int,
     ):
-        super().__init__(baseModel)
-        self.x_train = x_train
-        self.y_train = y_train
-        self.x_test = x_test
-        self.y_test = y_test
-        self.lossFunction = lossFunction
+        super().__init__(
+            baseModel=baseModel,
+            weight=weight,
+            bias=bias,
+            dimension=dimension,
+            size=size,
+        )
+        self.loss_func = loss_func
         self.metric = metric
-        self.errorThreshold = errorThreshold
-        self.truncatedRounds = truncatedRounds
-        self.seed = seed
-        self.truncatedNumber = truncatedNumber
-        self.x_distribution = None
-        self.y_distribution = None
-
-    def loadServerDataset(
-        self, x_distribution: torch.Tensor, y_distribution: torch.Tensor
-    ):
-        self.x_distribution = x_distribution
-        self.y_distribution = y_distribution
-
-    def calculateBaseScore(self):
-        y_pred = self.baseModel(self.x_test)
-        return self.metric(y_pred, self.y_test)
+        self.generate_dataset()
+        self.volume = None
 
     def train(self):
-        # d = GShapley(
-        #     x_train=self.x_train,
-        #     y_train=self.y_train,
-        #     x_test=self.x_test,
-        #     y_test=self.y_test,
-        #     baseModel=self.baseModel,
-        #     lossFunction=self.lossFunction,
-        #     metric=self.metric,
-        #     errorThreshold=self.errorThreshold,
-        #     truncatedRounds=100,
-        #     epoch=1,
-        #     seed=0,
-        # )
-        # d = DShapley(
-        #     x_train=self.x_train,
-        #     y_train=self.y_train,
-        #     x_test=self.x_test,
-        #     y_test=self.y_test,
-        #     x_dist=self.x_distribution,
-        #     y_dist=self.y_distribution,
-        #     baseModel=self.baseModel,
-        #     lossFunction=self.lossFunction,
-        #     metric=self.metric,
-        #     errorThreshold=self.errorThreshold,
-        #     truncatedRounds=100,
-        #     seed=0,
-        #     truncatedNumber=200,
-        # )
-        # d.shapley()
-        # self.baseModel.load_state_dict(d.modelParams)
-        LOOScore, modelParams = looScore(
-            x_train=self.x_train,
-            y_train=self.y_train,
-            x_test=self.x_test,
-            y_test=self.y_test,
-            baseModel=self.baseModel,
-            lossFunction=self.lossFunction,
-            metric=self.metric,
-            baseScore=self.calculateBaseScore(),
+        self.volume = Volume(dataset=self.dataset, omega=0.1)
+        self.volume.compute_volume()
+        self.volume.compute_robust_volumes()
+        self.baseModel = train_model(
+            baseModel=deepcopy(self.baseModel),
+            dataset=self.dataset,
+            loss_func=self.loss_func,
+            device=self.device,
+            epochs=3,
+            batchSize=128,
         )
-        self.baseModel.load_state_dict(modelParams)
 
     def receive(self, modelParams: dict):
         self.baseModel.load_state_dict(modelParams)
@@ -111,21 +90,19 @@ class Server(Node):
     def __init__(
         self,
         baseModel: torch.nn.Module,
-        x_test: torch.Tensor,
-        y_test: torch.Tensor,
-        x_distribution: torch.Tensor,
-        y_distribution: torch.Tensor,
         metric: torch.nn.Module,
+        dimension: int,
+        size: int,
         threshold: float,
     ):
-        super().__init__(baseModel)
-        self.x_test = x_test
-        self.y_test = y_test
-        self.x_distribution = x_distribution
-        self.y_distribution = y_distribution
+        super().__init__(
+            baseModel, weight=None, bias=None, dimension=dimension, size=size
+        )
         self.metric = metric
-        self.threshold = threshold
         self.clients = []
+        self.threshold = threshold
+        self.weight, self.bias = generate_weight_bias(d=self.dimension)
+        self.generate_dataset()
 
     def receive(self, modelsParams: list):
         keys = modelsParams[0].keys()
@@ -137,24 +114,26 @@ class Server(Node):
         self.baseModel.load_state_dict(newModelParams)
 
     def evaluate(self):
-        y_pred = self.baseModel(self.x_test)
-        return self.metric(y_pred, self.y_test)
+        return test_model(
+            dataset=self.dataset,
+            model=self.baseModel,
+            metric=self.metric,
+            device=self.device,
+            batchSize=128,
+        )
 
-    def selectClients(self, clients: list):
+    def select_clients(self, clients: list):
         self.clients = clients
 
     def train(self):
-        acc = 0
-        accs = []
-        while acc < self.threshold:
+        metric = math.inf
+        metrics = []
+        for _ in range(10):
+            # while metric > self.threshold:
             for client in self.clients:
                 client.receive(self.send())
-                client.loadServerDataset(
-                    x_distribution=self.x_distribution,
-                    y_distribution=self.y_distribution,
-                )
                 client.train()
             self.receive([client.send() for client in self.clients])
-            acc = float(self.evaluate().detach().cpu().numpy())
-            accs.append(acc)
-            print(accs)
+            metric = float(self.evaluate().detach().cpu().numpy())
+            metrics.append(metric)
+        print(metrics)
